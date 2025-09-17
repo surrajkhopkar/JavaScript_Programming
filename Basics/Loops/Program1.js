de4d976136d28fe1adebc335a44f76e4acd346f5
surrajkhopkar <contact@surrajkhopkar.com>
2025-09-18 00:26:19 +0530

    added basics

---
+++ Basics/Loops/Program1.js
@@ -0,0 +1,17 @@
+for(let i=0;i<5;i++){
+    console.log(`Hello World ${i}`)
+  
+}
+
+
+liverpool = ["Salah","Bobby","mane"]
+
+for(let player of liverpool){
+    console.log(player)
+}
+
+//ES6 syntax for each loop
+
+liverpool.forEach(player => {
+    console.log(player)
+});
